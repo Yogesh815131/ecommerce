94f57fad0e0124f4dca75db5aebebaa156c44eeb
Yogesh815131 <Yogesh@815131>
2024-07-20 12:15:13 +0530

    gitignore

--- backend/src/config/dbconnect.config.ts
+++ backend/src/config/dbconnect.config.ts
@@ -2,7 +2,7 @@
 import { connect, ConnectOptions } from 'mongoose';
 
 export const dbConnect = ()=>{
-    connect("mongodb+srv://ky815131:EwRSNRKzKFyqA62e@cluster0.mpjipo2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    connect("mongodb+srv://y32551849:QP08lNAxepksb0oy@cluster0.feburia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         {useNewUrlParser: true, useUnifiedTopology: true} as ConnectOptions).then(
         ()=>{
             console.log("Connected Successfully");            
