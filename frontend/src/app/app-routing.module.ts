3158de0a7d7b65a64b22a00fc9c347ea59c23b40
Yogesh815131 <ky815131@gmail.com>
2024-07-24 14:48:35 +0530

    food details page complete

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { FooddetailsComponent } from './components/pages/fooddetails/fooddetails.component';
 
 const routes: Routes = [
-  { path: '', component:HomeComponent},
-  { path: 'search/:searchterm', component:HomeComponent },
-  { path: 'tag/:tag', component:HomeComponent}
+  { path : '', component:HomeComponent},
+  { path : 'search/:searchterm', component:HomeComponent },
+  { path : 'tag/:tag', component:HomeComponent},
+  { path : 'food/:foodid', component:FooddetailsComponent }
 ];
 
 @NgModule({
--- frontend/src/app/components/pages/fooddetails/fooddetails.component.ts
+++ frontend/src/app/components/pages/fooddetails/fooddetails.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Food } from '../../../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from '../../../services/food.service';
 
 @Component({
   selector: 'app-fooddetails',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './fooddetails.component.css'
 })
 export class FooddetailsComponent {
-
+  food!:Food;
+  constructor(private activatedRoute:ActivatedRoute, private foodApi:FoodService, ) { 
+    this.activatedRoute.params.subscribe((params)=>{
+      console.log(params['foodid']);
+      
+      if (params['foodid']) {
+        this.foodApi.getFoodById(params['foodid']).subscribe((food_result:Food)=>{
+          this.food = food_result;
+        })
+      }
+    })
+  }
 }
